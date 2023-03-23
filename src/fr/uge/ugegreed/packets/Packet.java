5459a1532221e94d2d8bb080cca083995badc28e
Storm Wyvern <derkeethus96@gmail.com>
2023-03-23 17:28:42 +0100

    added enum for packet codes

--- src/fr/uge/ugegreed/packets/Packet.java
+++ src/fr/uge/ugegreed/packets/Packet.java
@@ -2,6 +2,29 @@ package fr.uge.ugegreed.packets;
 
 import java.nio.ByteBuffer;
 
-public interface Packet {
+/**
+ * Represents a TCP packet
+ */
+public sealed interface Packet permits InitPacket {
+  /**
+   * Defines the packet types and associated code
+   */
+  enum PacketCode {
+    INIT(1), UPDT(2), REQ(3), ACC(4), REF(5), ANS(6), REDI(7), DISC(8), OK_DISC(9);
+
+    private final int code;
+    PacketCode(int code) {
+      this.code = code;
+    }
+
+    byte getCode() {
+      return (byte) code;
+    }
+  }
+
+  /**
+   * Transforms the packet into ByteBuffer form, in a read state.
+   * @return ByteBuffer containing the packet
+   */
   ByteBuffer toBuffer();
 }
