b4433dc2b442d700ca68204061e22e2957235932
Storm Wyvern <derkeethus96@gmail.com>
2023-03-23 16:59:47 +0100

    added base code to Main and Controller classes

--- src/fr/uge/ugegreed/Controller.java
+++ src/fr/uge/ugegreed/Controller.java
@@ -1,4 +1,66 @@
 package fr.uge.ugegreed;
 
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.file.Path;
+import java.util.Objects;
+import java.util.logging.Logger;
+
 public class Controller {
+  private static final Logger logger = Logger.getLogger(Controller.class.getName());
+  private final Selector selector;
+  private final InetSocketAddress parentAddress;
+  private final Path resultPath;
+  private final int listenPort;
+  private final ServerSocketChannel serverSocketChannel;
+  private final SocketChannel parentSocketChannel;
+
+  public Controller(int listenPort, Path resultPath, InetSocketAddress parentAddress) throws IOException {
+    if (listenPort < 0 || listenPort > 65535) {
+      throw new IllegalArgumentException("Port number is incorrect");
+    }
+    Objects.requireNonNull(resultPath);
+
+    this.listenPort = listenPort;
+    this.selector = Selector.open();
+    this.resultPath = resultPath;
+    this.parentAddress = parentAddress;
+    this.serverSocketChannel = ServerSocketChannel.open();
+    serverSocketChannel.bind(new InetSocketAddress(listenPort));
+    this.parentSocketChannel = SocketChannel.open();
+  }
+  public Controller(int listenPort, Path resultPath) throws IOException {
+    this(listenPort, resultPath, null);
+  }
+
+  public void launch() throws IOException {
+    serverSocketChannel.configureBlocking(false);
+    serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+    logger.info("Listening on port " + listenPort);
+
+    if (parentAddress != null) {
+      parentSocketChannel.configureBlocking(false);
+      // TODO: create context + attach
+      parentSocketChannel.register(selector, SelectionKey.OP_CONNECT);
+      parentSocketChannel.connect(parentAddress);
+    }
+
+    // TODO: start console thread
+
+    while(!Thread.interrupted()) {
+      try {
+        selector.select(this::treatKey);
+      } catch (UncheckedIOException tunneled) {
+        throw tunneled.getCause();
+      }
+    }
+  }
+
+  private void treatKey(SelectionKey key) {
+  }
 }
--- src/fr/uge/ugegreed/Main.java
+++ src/fr/uge/ugegreed/Main.java
@@ -1,7 +1,34 @@
 package fr.uge.ugegreed;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.file.Path;
+import java.util.logging.Logger;
+
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
+  private static final Logger logger = Logger.getLogger(Main.class.getSimpleName());
+
+  private static void usage() {
+    System.out.println("Usage : UGEGreed port resultPath [ip port]");
+  }
+  public static void main(String[] args) throws IOException {
+    if (args.length != 2 && args.length != 4) {
+      usage();
+      return;
+    }
+
+    logger.info("Application start.");
+
+    var listenPort = Integer.parseInt(args[0]);
+    var resultPath = Path.of(args[1]);
+
+    if (args.length == 4) {
+      var parentAddress = new InetSocketAddress(args[2], Integer.parseInt(args[3]));
+      new Controller(listenPort, resultPath, parentAddress).launch();
+    } else {
+      new Controller(listenPort, resultPath).launch();
+    }
+
+    logger.info("Application closed.");
   }
 }
