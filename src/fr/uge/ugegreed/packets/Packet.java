a34707953eed1a8f893548865f0eba7962dbb71d
Storm Wyvern <derkeethus96@gmail.com>
2023-03-23 15:37:40 +0100

    added interfaces for Packets and Readers

---
+++ src/fr/uge/ugegreed/packets/Packet.java
@@ -0,0 +1,5 @@
+package fr.uge.ugegreed.packets;
+
+public interface Packet {
+
+}
---
+++ src/fr/uge/ugegreed/readers/Reader.java
@@ -0,0 +1,11 @@
+package fr.uge.ugegreed.readers;
+
+import java.nio.ByteBuffer;
+
+public interface Reader<T> {
+    public static enum ProcessStatus { DONE, REFILL, ERROR };
+
+    public ProcessStatus process(ByteBuffer bb);
+    public T get();
+    public void reset();
+}
