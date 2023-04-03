ab1cd1d2c8fe88a2f4d10a5c377f1428747d308b
Ofghanirre <aleronraider@gmail.com>
2023-04-03 20:15:58 +0200

    Fix implementation of Command subClass

--- src/fr/uge/ugegreed/commands/CommandDebug.java
+++ src/fr/uge/ugegreed/commands/CommandDebug.java
@@ -4,4 +4,13 @@ package fr.uge.ugegreed.commands;
  * Represents a debug console command
  */
 public record CommandDebug(CommandDebugCode debugCode) implements Command {
+    @Override
+    public CommandHelpData getHelp() {
+        return new CommandHelpData("DEBUG", new CommandHelpData.Argument[]{new CommandHelpData.Argument("code", "code for command : 1 : Potential")}, "Debug commands used to test code");
+    }
+
+    @Override
+    public String getName() {
+        return "DEBUG";
+    }
 }
