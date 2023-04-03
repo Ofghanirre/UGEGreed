37017f0a23673d6cc8db399f67e6289fca9ea928
Ofghanirre <aleronraider@gmail.com>
2023-04-03 20:16:21 +0200

    Integrated HelpCommand into Console

--- src/fr/uge/ugegreed/Console.java
+++ src/fr/uge/ugegreed/Console.java
@@ -1,10 +1,8 @@
 package fr.uge.ugegreed;
 
-import fr.uge.ugegreed.commands.CommandDebug;
-import fr.uge.ugegreed.commands.CommandDebugCode;
-import fr.uge.ugegreed.commands.CommandDisconnect;
-import fr.uge.ugegreed.commands.CommandStart;
+import fr.uge.ugegreed.commands.*;
 
+import java.util.Locale;
 import java.util.Objects;
 import java.util.Scanner;
 import java.util.logging.Logger;
@@ -35,10 +33,11 @@ public class Console {
         var line = scan.nextLine();
         var splitLine = line.split(" +");
         if (splitLine.length == 0) continue;
-        var result = switch (splitLine[0]) {
+        var result = switch (splitLine[0].toUpperCase(Locale.ROOT)) {
           case "START" -> sendStartCommand(splitLine);
           case "DISCONNECT" -> sendDisconnectCommand();
           case "DEBUG" -> sendDebugCommand(splitLine);
+          case "HELP" -> sendHelpCommand();
           default -> false;
         };
         if (!result) {
@@ -84,5 +83,10 @@ public class Console {
     } catch (NumberFormatException e) {
       return false;
     }
+  }
+
+  private boolean sendHelpCommand() throws InterruptedException {
+    controller.sendCommand(new CommandHelp());
+    return true;
   }
 }
