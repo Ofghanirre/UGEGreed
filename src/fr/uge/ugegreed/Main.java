35fa184c66506bb3ef97ae2cd72dd5b32a03f8e3
Ofghanirre <aleronraider@gmail.com>
2023-04-03 14:12:18 +0200

    Change error throwing to detailled logging on the Main

--- src/fr/uge/ugegreed/Main.java
+++ src/fr/uge/ugegreed/Main.java
@@ -3,6 +3,7 @@ package fr.uge.ugegreed;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.util.logging.Logger;
 
@@ -11,7 +12,7 @@ import java.util.logging.Logger;
  */
 public class Main {
   private static final Logger logger = Logger.getLogger(Main.class.getSimpleName());
-
+  private static int errorLogged;
   private static void usage() {
     System.out.println("Usage : UGEGreed port resultPath [ip port]");
   }
@@ -22,21 +23,44 @@ public class Main {
     }
 
     logger.info("Application start.");
-
-    var listenPort = Integer.parseInt(args[0]);
-    if (listenPort < 0 || listenPort > 65535) {
-      logger.warning("Port given <" + listenPort + "> is invalid, exiting...");
-      throw new IllegalArgumentException("Port given is invalid");
+    int listenPort = -1;
+    try {
+      listenPort = Integer.parseInt(args[0]);
+      if (listenPort < 0 || listenPort > 65535) {
+        logger.warning("port out of range:" + listenPort);
+        errorLogged += 1;
+      }
+    } catch (NumberFormatException e) {
+      logger.warning("The given port : " + args[0] + " is invalid");
+      errorLogged += 1;
+    }
+    
+    Path resultPath = null;
+    try {
+      resultPath = Path.of(args[1]);
+      if (!resultPath.toFile().exists()) {
+        logger.info("Result directory does not exists, creating it...");
+        Files.createDirectories(resultPath);
+      }
+    } catch (InvalidPathException e) {
+      logger.warning("The given result_path \"" + args[1] + "\" could not been resolved as a correct Path");
+      errorLogged += 1;
     }
 
-    var resultPath = Path.of(args[1]);
-    Files.createDirectories(resultPath);
-
+    InetSocketAddress parentAddress = null;
     if (args.length == 4) {
-      var parentAddress = new InetSocketAddress(args[2], Integer.parseInt(args[3]));
-      new Controller(listenPort, resultPath, parentAddress).launch();
+      try {
+        parentAddress = new InetSocketAddress(args[2], Integer.parseInt(args[3]));
+      } catch (IllegalArgumentException e) {
+        logger.warning(e.getMessage());
+        errorLogged += 1;
+      }
+    }
+
+    if (errorLogged != 0) {
+      logger.warning(errorLogged + ((errorLogged > 1) ? " errors were" : " error was") + " found while starting the Application, please fix them and restart the application");
     } else {
-      new Controller(listenPort, resultPath).launch();
+      new Controller(listenPort, resultPath, parentAddress).launch();
     }
 
     logger.info("Application closed.");
