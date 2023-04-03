87669a918ee44e1834498dea43ecd3ef24e1fc9a
Ofghanirre <aleronraider@gmail.com>
2023-04-03 18:47:23 +0200

    Refactoring CommandDebug to use the CommandDebugCode enum

--- src/fr/uge/ugegreed/commands/CommandDebug.java
+++ src/fr/uge/ugegreed/commands/CommandDebug.java
@@ -3,10 +3,5 @@ package fr.uge.ugegreed.commands;
 /**
  * Represents a debug console command
  */
-public record CommandDebug(int debugCode) implements Command {
-  public CommandDebug {
-    if (debugCode < 0) {
-      throw new IllegalArgumentException("debug code must be positive or null");
-    }
-  }
+public record CommandDebug(CommandDebugCode debugCode) implements Command {
 }
