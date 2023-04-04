5f5b755366e26e36976f7404b736ac32af9a0426
Ofghanirre <aleronraider@gmail.com>
2023-04-04 15:08:02 +0200

    Added a RequireNonNull

--- src/fr/uge/ugegreed/commands/CommandDebug.java
+++ src/fr/uge/ugegreed/commands/CommandDebug.java
@@ -1,9 +1,16 @@
 package fr.uge.ugegreed.commands;
 
+import java.util.Objects;
+
 /**
  * Represents a debug console command
  */
 public record CommandDebug(CommandDebugCode debugCode) implements Command {
+
+    public CommandDebug {
+        Objects.requireNonNull(debugCode);
+    }
+
     @Override
     public CommandHelpData getHelp() {
         return new CommandHelpData("DEBUG", new CommandHelpData.Argument[]{new CommandHelpData.Argument("code", "code for command : 1 : Potential")}, "Debug commands used to test code");
