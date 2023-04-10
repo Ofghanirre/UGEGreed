dd5243f16b5ad8b4a78fa0422ec714b42e1b6582
Ofghanirre <aleronraider@gmail.com>
2023-04-10 18:44:22 +0200

    Added HttpRequestParser L> getRequestFromURL L> testHttpHeaderCode

---
+++ src/fr/uge/ugegreed/utils/HttpRequestParser.java
@@ -0,0 +1,37 @@
+package fr.uge.ugegreed.utils;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.logging.Logger;
+
+public class HttpRequestParser {
+    public static String getRequestFromURL(String jarURL) throws MalformedURLException {
+        URL url = new URL(jarURL);
+        String host = url.getHost();
+        int port = url.getPort();
+        if (port == -1) port = 80;
+        String path = url.getPath();
+
+        return "GET " + path + " HTTP/1.1\r\n" +
+                "Host: " + host + ":" + port + "\r\n" +
+                "Accept: application/java-archive\r\n" +
+                "Connection: close" +
+                "\r\n";
+    }
+
+    public static boolean testHttpHeaderCode(int code, Logger logger) {
+        if (code == 200) return true;
+        String message = "HTTP Request resulted in code " + code + "; Cause:" +
+                switch (code) {
+                    case 400 -> "Bad Request";
+                    case 401 -> "Unauthorized";
+                    case 403 -> "Forbidden";
+                    case 404 -> "Not Found";
+                    case 500 -> "Internal Server Error";
+                    default -> " unknown";
+                };
+
+        logger.warning(message);
+        return false;
+    }
+}
