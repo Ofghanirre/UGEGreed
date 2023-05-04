6eb642b39c0ad1eb03110a90bd6bccc6ff7f36c5
Ofghanirre <aleronraider@gmail.com>
2023-05-04 17:43:35 +0200

    Added FixedThreadPool and argument to the Main execution

--- src/fr/uge/ugegreed/TaskExecutor.java
+++ src/fr/uge/ugegreed/TaskExecutor.java
@@ -8,12 +8,16 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public final class TaskExecutor {
+    private static int THREAD_AMOUNT = 16;
+
+    public static void setThreadAmount(int amount) { TaskExecutor.THREAD_AMOUNT = amount; }
+    public static int getThreadAmount() { return THREAD_AMOUNT; }
     private final ExecutorService executorService;
     private final ArrayBlockingQueue<AnsPacket> queue;
     public TaskExecutor(ArrayBlockingQueue<AnsPacket> queue) {
         Objects.requireNonNull(queue);
         this.queue = queue;
-        this.executorService = Executors.newCachedThreadPool();
+        this.executorService = Executors.newFixedThreadPool(THREAD_AMOUNT);
     }
     public void addJob(Checker checker, long job_id, long range_start, long range_end) {
         executorService.submit(() -> {
